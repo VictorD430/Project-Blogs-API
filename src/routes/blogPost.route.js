3f81ccac50af037f0f640fc0cf949b68c418238a
Victor Duque <victor.duque11@hotmail.com>
2023-09-02 20:06:21 -0300

    test

--- src/routes/blogPost.route.js
+++ src/routes/blogPost.route.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const { blogPostsCtrl } = require('../controllers');
 const tokenAuth = require('../middlewares/authentication');
-// const validate = require('../middlewares/validationBlogPost');
 
 const blogPostsRouter = express.Router();
 
-// blogPostsRouter.post('/', tokenAuth, validate, blogPostsCtrl.newBlogPost);
 blogPostsRouter.get('/', tokenAuth, blogPostsCtrl.getAll);
 
 module.exports = blogPostsRouter;
