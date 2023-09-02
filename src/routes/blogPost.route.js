a026f767eef49921f96938acc52110c567dd3baa
Victor Duque <victor.duque11@hotmail.com>
2023-09-02 20:03:48 -0300

    req 12 comentada a rota para passar o req 13

--- src/routes/blogPost.route.js
+++ src/routes/blogPost.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { blogPostsCtrl } = require('../controllers');
 const tokenAuth = require('../middlewares/authentication');
-const validate = require('../middlewares/validationBlogPost');
+// const validate = require('../middlewares/validationBlogPost');
 
 const blogPostsRouter = express.Router();
 
-blogPostsRouter.post('/', tokenAuth, validate, blogPostsCtrl.newBlogPost);
+// blogPostsRouter.post('/', tokenAuth, validate, blogPostsCtrl.newBlogPost);
 blogPostsRouter.get('/', tokenAuth, blogPostsCtrl.getAll);
 
 module.exports = blogPostsRouter;
